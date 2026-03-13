346503be5c9e1bb17091684ee2376d6e43c178a3
karabelnikov.s <karabelnikov.s@rwb.ru>
2026-03-13 03:54:54 +0300

    теперь все ходит на сервак

--- lib/core/config/app_env.dart
+++ lib/core/config/app_env.dart
@@ -13,15 +13,7 @@ class AppEnv {
       : _defaultBackendUrl();
 
   static String _defaultBackendUrl() {
-    if (!kDebugMode) return 'http://178.17.60.48:8080';
-
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        // Android emulator reaches host machine via 10.0.2.2.
-        return 'http://10.0.2.2:8080';
-      default:
-        return 'http://127.0.0.1:8080';
-    }
+    return 'http://84.252.140.117:8080';
   }
 
   static const String backendEmail = String.fromEnvironment(
